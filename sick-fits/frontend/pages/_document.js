1e7cf9d43bfed0cc3ff81213009b27ae59d33920
Govind Prathi <govind.prathi@gmail.com>
2021-06-11 20:56:49 +0000

    products page with gql query

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -4,14 +4,17 @@ import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
     const styleTags = sheet.getStyleElement();
-return { ...page, styleTags };
+    return { ...page, styleTags };
   }
+
   render() {
     return (
       <Html lang="en-CA">
-        <Head></Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
--- sick-fits/frontend/pages/products.js
+++ sick-fits/frontend/pages/products.js
@@ -1,5 +1,34 @@
+import {  useQuery } from '@apollo/client'
+import gql from 'graphql-tag'
+
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
 export default function products() {
+    const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY)
+    console.log(error,data,loading)
+    if (loading) {
+       return <div><p>Loading</p></div>
+    }
+    if (error) {
+        return <div><p>Error Loading Products</p></div>
+    }
     return (
-        <div> Products page</div>
+        <div> {data.allProducts.map((product) => <p>{product.name}</p>)}</div>
     )
 }
